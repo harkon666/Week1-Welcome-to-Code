789f7f106221a8b390c0c11df2bec2800b65aeae
Harkon666 <bryan.wicaksanaa@gmail.com>
2025-05-14 13:42:59 +0700

    push soal1

--- quiz/part1/soal1.js
+++ quiz/part1/soal1.js
@@ -2,7 +2,7 @@
 // Untuk memulai game itu diperlukan 2 variabel (untuk sekarang), 
 // yaitu nama dan peran. Variabel peran harus memiliki isi data, 
 // bila nama kosong, pemain akan diberikan peringatan berupa "nama wajib diisi"
-// bila kosong pemain akan diberikan peringatan berupa "Pilih Peranmu untuk memulai game". 
+// bila kosong pemain akan diberikan peringatan berupa "Pilih Peranmu untuk memulai game".
 // Terdapat 3 peran berbeda yaitu Ksatria, Tabib, dan Penyihir. 
 // Tugas Anda adalah untuk membuat program yang mengecek isi variabel 
 // peran serta mengeluarkan respon sesuai isi variabel tersebut.
@@ -18,20 +18,21 @@
 // algoritma
 //isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
 
-let nama = "Test", peran = "penyihir";
+let nama = "harkon"
+let peran = "asdasda"
 
-if (nama === "") {
-  console.log("nama wajib diisi")
-} else if (peran === "") {
-  console.log("Pilih Peranmu untuk memulai game")
+if (nama === '') {
+  console.log('nama wajib diisi')
+} else if (peran === '') {
+  console.log('Pilih Peranmu untuk memulai game')
 } else {
-  if (peran === "ksatria") {
+  if (peran === 'ksatria') {
     console.log(`halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu!`)
+  } else if (peran === 'tabib') {
+    console.log(`halo Tabib ${nama} , kamu akan membantu temanmu yang terluka!`)
   } else if (peran === 'penyihir') {
-    console.log(`Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!`)
-  } else if (peran === "tabib") {
-    console.log(`halo Tabib ${nama} , kamu akan membantu temanmu yang terluka`)
+    console.log(`halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!`)
   } else {
-    console.log("tapi kayaknya kamu jadi bot aja ya")
+    console.log('kayaknya kamu jadi bot aja ya')
   }
 }
